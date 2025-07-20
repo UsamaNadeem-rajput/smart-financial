dc6ce9b948ce13c3ca82fad1b4da24c9e5cda03a
Usama Nadeem <rajputusama849@gmail.com>
2025-07-21 03:15:16 +0500

    back-end changes

--- server/server.js
+++ server/server.js
@@ -1,69 +1,3 @@
-// // server.js
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const session = require('express-session');
-// const MySQLStore = require('express-mysql-session')(session);
-// const pool = require('./db');
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-
-// /* ---------- Middleware ---------- */
-// app.use(
-//   cors({
-//     origin: ['http://localhost:5173','https://alamsherbaloch.com'], // Hostinger front-end
-//     credentials: true,
-//   })
-// );
-// app.use(express.json());
-
-// app.use(
-//   session({
-//     key: 'connect.sid',
-//     secret: process.env.SESSION_SECRET,
-//     store: sessionStore,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       httpOnly: true,
-//       secure: true,          // required for HTTPS
-//       sameSite: 'None',      // required for cross-origin cookies
-//       maxAge: 24 * 60 * 60 * 1000, // 1 day
-//     },
-//   })
-// );
-
-// // Routes
-// const registerRoute = require('./routers/register');
-// const loginRoute = require('./routers/login');
-// const businessRoute = require('./routers/business');
-// const showbusinessesRoute = require('./routers/showbusinesses');
-// const createnewaccount = require('./routers/createnewaccount');
-// const list = require('./routers/list');
-// const searchAccounts = require('./routers/searchAccounts');
-// const transaction = require('./routers/transactionApi');
-// const showTransactions = require('./routers/showtransactions');
-// const authRoutes = require('./routers/auth');
-// app.use('/', authRoutes);
-// app.use('/api', showTransactions);
-// app.use('/api/accounts-list', list);
-// app.use('/api/createnewaccount', createnewaccount);
-// app.use('/api/showbusinesses', showbusinessesRoute);
-// app.use('/api/business', businessRoute);
-// app.use('/api/register', registerRoute);
-// app.use('/api/login', loginRoute);
-// app.use('/api/search-accounts', searchAccounts);
-// app.use('/api', transaction);
-
-
-
-
-// /* ---------- Start ---------- */
-// app.listen(PORT, () => console.log(`✅ Server running on :${PORT}`));
-
-
 // server.js
 require('dotenv').config();
 const express = require('express');
@@ -73,7 +7,7 @@ const MySQLStore = require('express-mysql-session')(session);
 const pool = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 /* ---------- Session store ---------- */
 const sessionStore = new MySQLStore(
