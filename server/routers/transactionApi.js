1c15869042af9e6ce61aedb5e4b8bebf5ff7b226
Usama Nadeem <rajputusama849@gmail.com>
2025-07-22 23:09:36 +0500

    Add animation

--- server/routers/transactionApi.js
+++ server/routers/transactionApi.js
@@ -31,7 +31,7 @@ router.post('/transactions', async (req, res) => {
         if (transaction_id) {
             // Insert with provided transaction_id
             [insertResult] = await conn.execute(
-                'INSERT INTO transactions (transaction_id, business_id, description, debit, credit, date) VALUES (?, ?, ?, ?, ?, ?)',
+                'INSERT INTO transactions (id_from_front_end, business_id, description, debit, credit, date) VALUES (?, ?, ?, ?, ?, ?)',
                 [transaction_id, business_id, description || '', debit, credit, date]
             );
         } else {
